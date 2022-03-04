95b563a287718a88f15585c1c5ae281ce39fdb15
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-03-04 16:10:41 +0100

    Changed PK of user table to uuid

---
+++ app/Traits/HasUuid.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+
+trait HasUuid
+{
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+
+            $model->keyType = 'string';
+
+            $model->incrementing = false;
+
+            $model->{$model->getKeyName()} = $model->{$model->getKeyName()} ?? Str::orderedUuid();
+        });
+    }
+
+    public function getIncrementing(): bool
+    {
+        return false;
+    }
+
+    public function getKeyType(): string
+    {
+        return 'string';
+    }
+}
