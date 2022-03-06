89ebba921173ed128d274c26c766d3782b78565c
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-03-06 15:41:15 +0100

    Fix validation in form requests

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -4,7 +4,10 @@ declare(strict_types = 1);
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 
 class LoginRequest extends FormRequest
 {
@@ -30,5 +33,23 @@ class LoginRequest extends FormRequest
             'password' => 'string|required',
             'device_name' => 'string|in:desktop,phone,web|required'
         ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        $response = new JsonResponse(
+            $validator->errors(),
+            422
+        );
+
+        throw new HttpResponseException($response, 422);
     }
 }
