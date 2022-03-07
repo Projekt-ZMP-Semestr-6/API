d808d1466fa6f8d1a5a8d78d8dd09c3f73c0086d
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-03-07 14:42:29 +0100

    Create routes for email verification

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\EmailVerificationController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum', 'verified'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
@@ -25,4 +27,11 @@ Route::prefix('/auth')->group(function () {
     Route::post('/register', RegisterController::class);
     Route::post('/login', LoginController::class);
     Route::get('/logout', LogoutController::class)->middleware('auth:sanctum');
+
+    Route::prefix('/email')->controller(EmailVerificationController::class)->group(function () {
+        Route::get('/verify', 'notice')->middleware('auth:sanctum')->name('verification.notice');
+        Route::get('/verify/{id}/{hash}', 'verify')->middleware(['auth:sanctum', 'signed'])->name('verification.verify');
+        Route::post('/verification-notification', 'resendMail')->middleware(['auth:sanctum', 'throttle:3,1'])->name('verification.send');
+    });
 });
+
