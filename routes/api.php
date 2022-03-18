02c13aabac4e6308cc5c039343e88a6903929b06
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-03-18 08:47:18 +0100

    Added routes for resetting password

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\Auth\EmailVerificationController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Requests\ResetPasswordRequest;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -32,6 +35,11 @@ Route::prefix('/auth')->group(function () {
         Route::get('/verify', 'notice')->middleware('auth:sanctum')->name('verification.notice');
         Route::get('/verify/{id}/{hash}', 'verify')->middleware(['auth:sanctum', 'signed'])->name('verification.verify');
         Route::post('/verification-notification', 'resendMail')->middleware(['auth:sanctum', 'throttle:3,1'])->name('verification.send');
+    });
+
+    Route::controller(ResetPasswordController::class)->middleware('guest')->group(function () {
+        Route::post('/forgot-password', 'sendNotification')->name('password.email');
+        Route::post('/reset-password', 'resetPassword')->name('password.update');
     });
 });
 
