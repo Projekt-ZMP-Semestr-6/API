59a29783ee456711b5dc9f7974adc27a10dd949c
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-03-21 13:41:55 +0100

    Added swagger annotations

--- app/Http/Controllers/ChangeNameController.php
+++ app/Http/Controllers/ChangeNameController.php
@@ -7,6 +7,49 @@ use App\Http\Requests\ChangeNameRequest;
 use Illuminate\Http\JsonResponse;
 use Throwable;
 
+/**
+ * @OA\Post(
+ * path="/api/user/name",
+ * summary="Change name",
+ * description="Change user's name",
+ * operationId="userName",
+ * tags={"User"},
+ * security={{"sanctum": {}}},
+ * @OA\RequestBody(
+ *      description="Pass name to set",
+ *      @OA\JsonContent(
+ *          required={"name"},
+ *          @OA\Property(
+ *              property="name",
+ *              type="string",
+ *              example="Bob4",
+ *          ),
+ *      ),
+ * ),
+ * @OA\Response(
+ *      response=200,
+ *      description="OK",
+ *      @OA\JsonContent(
+ *          type="string",
+ *          example="Name updated!",
+ *      ),
+ * ),
+ * @OA\Response(
+ *      response=422,
+ *      description="Unprocessable Content",
+ *      @OA\JsonContent(
+ *          @OA\Property(
+ *              property="name",
+ *              type="array",
+ *              @OA\Items(
+ *                  type="string",
+ *                  example="problem_message",
+ *              ),
+ *          ),
+ *      ),
+ * ),
+ * )
+ */
 class ChangeNameController extends Controller
 {
     public function __invoke(ChangeNameRequest $request): JsonResponse
