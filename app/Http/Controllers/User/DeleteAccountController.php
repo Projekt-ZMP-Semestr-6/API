51b938917a18c4ab42bc17875868215ecfdc1228
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-03-21 19:45:36 +0100

    Added swagger annotations

--- app/Http/Controllers/User/DeleteAccountController.php
+++ app/Http/Controllers/User/DeleteAccountController.php
@@ -8,6 +8,49 @@ use App\Http\Requests\User\DeleteAccountRequest;
 use Illuminate\Http\JsonResponse;
 use Throwable;
 
+/**
+ * @OA\Post(
+ * path="/api/user/delete",
+ * summary="Delete account",
+ * description="Delete currently authenticated user",
+ * operationId="userDelete",
+ * tags={"User"},
+ * security={{"sanctum": {}}},
+ * @OA\RequestBody(
+ *      description="Pass password to confirm deletion",
+ *      @OA\JsonContent(
+ *          required={"password"},
+ *          @OA\Property(
+ *              property="password",
+ *              type="password",
+ *              example="password123",
+ *          ),
+ *      ),
+ * ),
+ * @OA\Response(
+ *      response=200,
+ *      description="OK",
+ *      @OA\JsonContent(
+ *          type="string",
+ *          example="Account deleted!",
+ *      ),
+ * ),
+ * @OA\Response(
+ *      response=422,
+ *      description="Unprocessable Content",
+ *      @OA\JsonContent(
+ *          @OA\Property(
+ *              property="password",
+ *              type="array",
+ *              @OA\Items(
+ *                  type="string",
+ *                  example="problem_message",
+ *              ),
+ *          ),
+ *      ),
+ * ),
+ * )
+ */
 class DeleteAccountController extends Controller
 {
     public function __invoke(DeleteAccountRequest $request): JsonResponse
