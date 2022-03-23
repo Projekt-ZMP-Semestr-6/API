9c50270954b5865b66a2933c257eef43266f5768
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-03-23 12:20:30 +0100

    Refactored form request

--- app/Http/Requests/User/UpdateEmailRequest.php
+++ app/Http/Requests/User/UpdateEmailRequest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Http\Requests\User;
 
 use Illuminate\Contracts\Validation\Validator;
@@ -7,14 +9,14 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
 
-class ChangeEmailRequest extends FormRequest
+class UpdateEmailRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -24,7 +26,7 @@ class ChangeEmailRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'password' => 'string|required|current_password:sanctum',
