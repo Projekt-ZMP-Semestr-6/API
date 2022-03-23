83ed100d98e280234a771c55eb82ca6c7d0b07e4
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-03-23 11:59:25 +0100

    Added `ChangeEmailRequest`

---
+++ app/Http/Requests/User/ChangeEmailRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class ChangeEmailRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'password' => 'string|required|current_password:sanctum',
+            'email' => 'email|required',
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        $response = new JsonResponse(
+            $validator->errors(),
+            422
+        );
+
+        throw new HttpResponseException($response);
+    }
+}
