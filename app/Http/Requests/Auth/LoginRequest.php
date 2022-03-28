3f7527dbe8ab772469e7f280e6dd81f0efe0e4e5
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-03-28 19:47:16 +0200

    Added checking if email is verified logic to `LoginRequest` and updated rules

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace App\Http\Requests\Auth;
 
+use App\Exceptions\Auth\EmailNotVerifiedException;
+use App\Models\User;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
@@ -19,10 +21,20 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'email|required',
+            'email' => 'email|required|exists:users,email',
             'password' => 'string|required',
             'device_name' => 'string|in:desktop,phone,web|required'
         ];
+    }
+
+    protected function passedValidation(): void
+    {
+        $email = $this->validated('email');
+        $user = User::whereEmail($email)->first();
+
+        if(! $user->hasVerifiedEmail()) {
+            throw new EmailNotVerifiedException;
+        };
     }
 
     protected function failedValidation(Validator $validator): void
