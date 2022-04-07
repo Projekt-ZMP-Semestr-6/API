ce563b27de226c45df4ae9108219aefb16e75907
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-04-07 11:00:25 +0200

    Created `FreebiesController`

---
+++ app/Http/Controllers/Game/FreebiesController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Http\Controllers\Game;
+
+use App\Http\Controllers\Controller;
+
+class FreebiesController extends Controller
+{
+    public function __invoke()
+    {
+        //
+    }
+}
