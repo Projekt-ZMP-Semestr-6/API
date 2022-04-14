2767ccc8d39dbc8b0432623970e3d9dea25c35ae
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-04-14 17:12:56 +0200

    Added `GamesNotFetchedException`

---
+++ app/Exceptions/Game/GamesNotFetchedException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Exceptions\Game;
+
+use Exception;
+
+class GamesNotFetchedException extends Exception
+{
+    public function __construct()
+    {
+        parent::__construct('The problem occured while tried to fetch games', 500);
+    }
+}
