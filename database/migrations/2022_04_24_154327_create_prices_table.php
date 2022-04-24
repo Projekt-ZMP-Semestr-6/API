2e0954648ab36bc688484a9a6b5f5b351df3e4d8
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-04-24 19:02:33 +0200

    Added `prices` table

---
+++ database/migrations/2022_04_24_154327_create_prices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('game_id');
+            $table->integer('actual_price')->nullable();
+            $table->integer('lowest_price')->nullable();
+            $table->integer('highest_price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+};
