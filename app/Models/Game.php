b8afd8db57d83a362cb28a24faed9a5146c18551
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-04-24 11:40:20 +0200

    Updated and generated api docs

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -12,9 +12,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
  * @OA\Schema(
  * schema="Game",
  * type="object",
- * @OA\Property(property="id", type="string", example="487"),
+ * @OA\Property(property="id", type="int", example="487"),
  * @OA\Property(property="name", type="string", example="LEGO® Batman™: The Videogame"),
- * @OA\Property(property="appid", type="string", example="21000"),
+ * @OA\Property(property="appid", type="int", example="21000"),
  * @OA\Property(property="last_modified", type="date:unix_timestamp", example="1573509038"),
  * @OA\Property(property="header_img", type="string", example="https://cdn.cloudflare.steamstatic.com/steam/apps/21000/capsule_184x69.jpg?t=1573509038"),
  * )
