d57f577774b384f35661068ee344869ff1b575f7
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-04-25 19:48:03 +0200

    Split `prices` table into `lowest_prices`, `highest_prices` and `actual_prices`

--- database/migrations/2022_04_25_090925_create_lowest_prices_table.php
+++ database/migrations/2022_04_25_090925_create_lowest_prices_table.php
@@ -13,12 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('prices', function (Blueprint $table) {
+        Schema::create('lowest_prices', function (Blueprint $table) {
             $table->id();
             $table->foreignId('game_id');
-            $table->integer('actual_price')->nullable();
-            $table->integer('lowest_price')->nullable();
-            $table->integer('highest_price')->nullable();
+            $table->integer('price')->nullable();
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('prices');
+        Schema::dropIfExists('lowest_prices');
     }
 };
